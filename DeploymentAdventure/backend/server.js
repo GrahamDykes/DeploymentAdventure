28f4d89865cb6da841d5e57d859b505750b2b0ec
GrahamDykes <grahamdykes@gmail.com>
2025-04-18 16:42:03 -0700

    following such a dumb tutorial

--- DeploymentAdventure/backend/server.js
+++ DeploymentAdventure/backend/server.js
@@ -8,8 +8,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Connected: " + PORT);
+// app.get("/", (req, res) => {
+//   res.send("Connected: " + PORT);
+// });
+
+//Im not sure this is wtf dude is talking about
+//ok, so this blanks out one entry after .com.  transforming cryptic.llc/backend/letswritecode to cryptic.llc/letwrite code.  so to get to /backend, you would have to put in cryptic.llc/backend/backend.
+app.use((req, res, next) => {
+  req.url = req.url.replace(/^\/[^\/]+/, "");
+  next();
+});
+
+app.get("/backend", (req, res) => {
+  res.send("Backend is connected! @ " + PORT);
+});
+
+app.get("/letswritecode", (req, res) => {
+  res.send("Following dummy's instructions @ " + PORT);
 });
 
 app.listen(PORT, () => {
